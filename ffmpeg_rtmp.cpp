923c164345415a06b7e4a8fec7ac28a189863df7
turkay.biliyor <turkay.biliyor@iys.org.tr>
2023-05-15 20:59:34 +0300

    ffmpeg

--- ffmpeg_rtmp.cpp
+++ ffmpeg_rtmp.cpp
@@ -3,57 +3,238 @@
 ffmpeg_rtmp::ffmpeg_rtmp(QObject *parent)
     : QThread{parent}
 {
-    in_filename  = "rtmp://0.0.0.0:8889/live/app";
-    //    av_register_all();
-    //    avcodec_register_all();
+    in_filename  = "rtmp://192.168.1.7:8889/live/app";
+    out_filename = "receive.flv";
     avformat_network_init();
 }
 
 void ffmpeg_rtmp::stop()
 {
+    av_packet_free(&packet);
+    for (i = 0; i < ifmt_ctx->nb_streams; i++) {
+        avcodec_free_context(&stream_ctx[i].dec_ctx);
+        if (ofmt_ctx && ofmt_ctx->nb_streams > i && ofmt_ctx->streams[i] && stream_ctx[i].enc_ctx)
+            avcodec_free_context(&stream_ctx[i].enc_ctx);
+        av_frame_free(&stream_ctx[i].dec_frame);
+    }
+    av_free(stream_ctx);
     avformat_close_input(&ifmt_ctx);
+    if (ofmt_ctx && !(ofmt_ctx->oformat->flags & AVFMT_NOFILE))
+        avio_closep(&ofmt_ctx->pb);
+    avformat_free_context(ofmt_ctx);
+
+    if (ret < 0)
+        av_log(NULL, AV_LOG_ERROR, "Error occurred: %s\n", av_err2str(ret));
+
     sendInfo("Stream thread stopped.");
+}
+
+int ffmpeg_rtmp::open_input_file(const char *filename)
+{
+    AVDictionary *format_opts = NULL;
+    av_dict_set(&format_opts, "timeout", "10", 0);
+
+    if ((ret = avformat_open_input(&ifmt_ctx, filename, NULL ,&format_opts)) < 0)
+    {
+        av_strerror(ret, buff, 256);
+        sendInfo(buff);
+        av_log(NULL, AV_LOG_ERROR, "Cannot open input file\n");
+        return ret;
+    }
+
+    if ((ret = avformat_find_stream_info(ifmt_ctx, NULL)) < 0) {
+        av_strerror(ret, buff, 256);
+        sendInfo(buff);
+        av_log(NULL, AV_LOG_ERROR, "Cannot find stream information\n");
+        return ret;
+    }
+
+    stream_ctx = (StreamContext*)av_calloc(ifmt_ctx->nb_streams, sizeof(*stream_ctx));
+    if (!stream_ctx)
+        return AVERROR(ENOMEM);
+
+    for (i = 0; i < ifmt_ctx->nb_streams; i++) {
+        AVStream *stream = ifmt_ctx->streams[i];
+        const AVCodec *dec = avcodec_find_decoder(stream->codecpar->codec_id);
+        AVCodecContext *codec_ctx;
+        if (!dec) {
+            av_log(NULL, AV_LOG_ERROR, "Failed to find decoder for stream #%u\n", i);
+            return AVERROR_DECODER_NOT_FOUND;
+        }
+        codec_ctx = avcodec_alloc_context3(dec);
+        if (!codec_ctx) {
+            av_log(NULL, AV_LOG_ERROR, "Failed to allocate the decoder context for stream #%u\n", i);
+            return AVERROR(ENOMEM);
+        }
+        ret = avcodec_parameters_to_context(codec_ctx, stream->codecpar);
+        if (ret < 0) {
+            av_log(NULL, AV_LOG_ERROR, "Failed to copy decoder parameters to input decoder context "
+                                       "for stream #%u\n", i);
+            return ret;
+        }
+        /* Reencode video & audio and remux subtitles etc. */
+        if (codec_ctx->codec_type == AVMEDIA_TYPE_VIDEO
+                || codec_ctx->codec_type == AVMEDIA_TYPE_AUDIO) {
+            if (codec_ctx->codec_type == AVMEDIA_TYPE_VIDEO)
+                codec_ctx->framerate = av_guess_frame_rate(ifmt_ctx, stream, NULL);
+            /* Open decoder */
+            ret = avcodec_open2(codec_ctx, dec, NULL);
+            if (ret < 0) {
+                av_log(NULL, AV_LOG_ERROR, "Failed to open decoder for stream #%u\n", i);
+                return ret;
+            }
+        }
+        stream_ctx[i].dec_ctx = codec_ctx;
+
+        stream_ctx[i].dec_frame = av_frame_alloc();
+        if (!stream_ctx[i].dec_frame)
+            return AVERROR(ENOMEM);
+    }
+    av_dump_format(ifmt_ctx, 0, filename, 0);
+    return 0;
+}
+
+int ffmpeg_rtmp::open_output_file(const char *filename)
+{
+    AVStream *out_stream;
+    AVStream *in_stream;
+    AVCodecContext *dec_ctx, *enc_ctx;
+    const AVCodec *encoder;
+    int ret;
+    unsigned int i;
+
+    ofmt_ctx = NULL;
+    avformat_alloc_output_context2(&ofmt_ctx, NULL, NULL, filename);
+    if (!ofmt_ctx) {
+        av_log(NULL, AV_LOG_ERROR, "Could not create output context\n");
+        return AVERROR_UNKNOWN;
+    }
+
+
+    for (i = 0; i < ifmt_ctx->nb_streams; i++) {
+        out_stream = avformat_new_stream(ofmt_ctx, NULL);
+        if (!out_stream) {
+            av_log(NULL, AV_LOG_ERROR, "Failed allocating output stream\n");
+            return AVERROR_UNKNOWN;
+        }
+
+        in_stream = ifmt_ctx->streams[i];
+        dec_ctx = stream_ctx[i].dec_ctx;
+
+        if (dec_ctx->codec_type == AVMEDIA_TYPE_VIDEO
+                || dec_ctx->codec_type == AVMEDIA_TYPE_AUDIO) {
+            /* in this example, we choose transcoding to same codec */
+            encoder = avcodec_find_encoder(dec_ctx->codec_id);
+            if (!encoder) {
+                av_log(NULL, AV_LOG_FATAL, "Necessary encoder not found\n");
+                return AVERROR_INVALIDDATA;
+            }
+            enc_ctx = avcodec_alloc_context3(encoder);
+            if (!enc_ctx) {
+                av_log(NULL, AV_LOG_FATAL, "Failed to allocate the encoder context\n");
+                return AVERROR(ENOMEM);
+            }
+
+            /* In this example, we transcode to same properties (picture size,
+                * sample rate etc.). These properties can be changed for output
+                * streams easily using filters */
+            if (dec_ctx->codec_type == AVMEDIA_TYPE_VIDEO) {
+                enc_ctx->height = dec_ctx->height;
+                enc_ctx->width = dec_ctx->width;
+                enc_ctx->sample_aspect_ratio = dec_ctx->sample_aspect_ratio;
+                /* take first format from list of supported formats */
+                if (encoder->pix_fmts)
+                    enc_ctx->pix_fmt = encoder->pix_fmts[0];
+                else
+                    enc_ctx->pix_fmt = dec_ctx->pix_fmt;
+                /* video time_base can be set to whatever is handy and supported by encoder */
+                enc_ctx->time_base = av_inv_q(dec_ctx->framerate);
+            } else {
+                enc_ctx->sample_rate = dec_ctx->sample_rate;
+                ret = av_channel_layout_copy(&enc_ctx->ch_layout, &dec_ctx->ch_layout);
+                if (ret < 0)
+                    return ret;
+                /* take first format from list of supported formats */
+                enc_ctx->sample_fmt = encoder->sample_fmts[0];
+                enc_ctx->time_base = (AVRational){1, enc_ctx->sample_rate};
+            }
+
+            if (ofmt_ctx->oformat->flags & AVFMT_GLOBALHEADER)
+                enc_ctx->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
+
+            /* Third parameter can be used to pass settings to encoder */
+            ret = avcodec_open2(enc_ctx, encoder, NULL);
+            if (ret < 0) {
+                av_log(NULL, AV_LOG_ERROR, "Cannot open video encoder for stream #%u\n", i);
+                return ret;
+            }
+            ret = avcodec_parameters_from_context(out_stream->codecpar, enc_ctx);
+            if (ret < 0) {
+                av_log(NULL, AV_LOG_ERROR, "Failed to copy encoder parameters to output stream #%u\n", i);
+                return ret;
+            }
+
+            out_stream->time_base = enc_ctx->time_base;
+            stream_ctx[i].enc_ctx = enc_ctx;
+        } else if (dec_ctx->codec_type == AVMEDIA_TYPE_UNKNOWN) {
+            av_log(NULL, AV_LOG_FATAL, "Elementary stream #%d is of unknown type, cannot proceed\n", i);
+            return AVERROR_INVALIDDATA;
+        } else {
+            /* if this stream must be remuxed */
+            ret = avcodec_parameters_copy(out_stream->codecpar, in_stream->codecpar);
+            if (ret < 0) {
+                av_log(NULL, AV_LOG_ERROR, "Copying parameters for stream #%u failed\n", i);
+                return ret;
+            }
+            out_stream->time_base = in_stream->time_base;
+        }
+
+    }
+    av_dump_format(ofmt_ctx, 0, filename, 1);
+
+    if (!(ofmt_ctx->oformat->flags & AVFMT_NOFILE)) {
+        ret = avio_open(&ofmt_ctx->pb, filename, AVIO_FLAG_WRITE);
+        if (ret < 0) {
+            av_log(NULL, AV_LOG_ERROR, "Could not open output file '%s'", filename);
+            return ret;
+        }
+    }
+
+    /* init muxer, write output file header */
+    ret = avformat_write_header(ofmt_ctx, NULL);
+    if (ret < 0) {
+        av_log(NULL, AV_LOG_ERROR, "Error occurred when opening output file\n");
+        return ret;
+    }
+
+    return 0;
 }
 
 void ffmpeg_rtmp::run()
 {
     sendInfo("Trying to create rtmp stream server.");
-
-    AVDictionary *format_opts = NULL;
-    av_dict_set(&format_opts, "timeout", "3", 0);
-
-    if ((ret = avformat_open_input(&ifmt_ctx, in_filename, NULL ,&format_opts)) < 0)
-    {
-        char buff[256];
-        av_strerror(ret, buff, 256);
-        sendInfo(buff);
-        sendInfo("Cannot open input file.");
+    if ((ret = open_input_file(in_filename)) < 0)
         return;
-    }
-
-    if ((ret = avformat_find_stream_info(ifmt_ctx, NULL)) < 0) {
-        sendInfo( "Cannot find stream information.");
+    if ((ret = open_output_file(out_filename)) < 0)
         return;
-    }
+    if (!(packet = av_packet_alloc()))
+        return;
 
-    for (i = 0; i < ifmt_ctx->nb_streams; i++) {
-        AVStream *stream;
-        AVCodecContext *codec_ctx;
-        stream = ifmt_ctx->streams[i];
-        codec_ctx = stream->codec;
-        /* Reencode video & audio and remux subtitles etc. */
-        if (codec_ctx->codec_type == AVMEDIA_TYPE_VIDEO
-                || codec_ctx->codec_type == AVMEDIA_TYPE_AUDIO) {
-            /* Open decoder */
-            ret = avcodec_open2(codec_ctx,
-                                avcodec_find_decoder(codec_ctx->codec_id), NULL);
-            if (ret < 0) {
-                av_log(NULL, AV_LOG_ERROR, "Failed to open decoder for stream #%u\n", i);
-                return;
-            }
+    /* read all packets */
+    while (true) {
+        AVStream *in_stream, *out_stream;
+        if ((ret = av_read_frame(ifmt_ctx, packet)) < 0)
+        {
+            continue;
         }
-    }
-    av_dump_format(ifmt_ctx, 0, in_filename, 0);
+        stream_index = packet->stream_index;
+        in_stream  = ifmt_ctx->streams[packet->stream_index];
+        out_stream = ofmt_ctx->streams[packet->stream_index];
 
-    sendInfo("Rtmp stream server created.");
+        AVRational *time_base = &ifmt_ctx->streams[packet->stream_index]->time_base;
+        qDebug() << packet->pts << packet->duration;
+
+        QThread::msleep(10);
+    }
+    av_write_trailer(ofmt_ctx);
 }
--- ffmpeg_rtmp.h
+++ ffmpeg_rtmp.h
@@ -19,9 +19,13 @@
 extern "C"
 {
 #include <libavcodec/avcodec.h>
-#include "libavformat/avformat.h"
-#include "libavutil/mathematics.h"
-#include "libavutil/time.h"
+#include <libavformat/avformat.h>
+#include <libavfilter/buffersink.h>
+#include <libavfilter/buffersrc.h>
+#include <libavutil/channel_layout.h>
+#include <libavutil/opt.h>
+#include <libavutil/pixdesc.h>
+#include <libavformat/avio.h>
 };
 #else
 //Linux...
@@ -31,15 +35,42 @@ extern "C"
 #endif
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
-#include <libavutil/mathematics.h>
-#include <libavutil/time.h>
+#include <libavfilter/buffersink.h>
+#include <libavfilter/buffersrc.h>
+#include <libavutil/channel_layout.h>
+#include <libavutil/opt.h>
+#include <libavutil/pixdesc.h>
+#include <libavformat/avio.h>
 #ifdef __cplusplus
 };
 #endif
 #endif
 
-//'1': Use H.264 Bitstream Filter
-#define USE_H264BSF 0
+struct buffer_data
+{
+    uint8_t *buf;
+    size_t size;
+    uint8_t *ptr;
+    size_t room; ///< size left in the buffer
+};
+
+typedef struct FilteringContext {
+    AVFilterContext *buffersink_ctx;
+    AVFilterContext *buffersrc_ctx;
+    AVFilterGraph *filter_graph;
+
+    AVPacket *enc_pkt;
+    AVFrame *filtered_frame;
+} FilteringContext;
+
+
+typedef struct StreamContext {
+    AVCodecContext *dec_ctx;
+    AVCodecContext *enc_ctx;
+
+    AVFrame *dec_frame;
+} StreamContext;
+
 
 class ffmpeg_rtmp : public QThread
 {
@@ -48,15 +79,21 @@ public:
     explicit ffmpeg_rtmp(QObject *parent = nullptr);
     void stop();
 private:
-    AVOutputFormat *ofmt = NULL;
-    //Input AVFormatContext and Output AVFormatContext
-    AVFormatContext *ifmt_ctx = NULL, *ofmt_ctx = NULL;
-    AVPacket pkt;
-    const char *in_filename, *out_filename;
-    int ret, i;
+    int open_input_file(const char *filename);
+    int open_output_file(const char *filename);    
+
+    int ret;
+    AVPacket *packet = NULL;
+    unsigned int stream_index;
+    unsigned int i;
+    char buff[256];
     int videoindex=-1;
     int frame_index=0;
 
+    //Input AVFormatContext and Output AVFormatContext
+    AVFormatContext *ifmt_ctx = NULL, *ofmt_ctx = NULL;
+    StreamContext *stream_ctx;
+    const char *in_filename, *out_filename;
 protected:
     void run();
 
